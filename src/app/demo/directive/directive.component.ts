9044598031b51041e464554a0fe72f77436479df
shakespace <zez160089@163.com>
2023-07-08 15:47:41 +0800

    3.3 属性型指令 ngClass ngStyle ng-content初步使用

--- src/app/demo/directive/directive.component.ts
+++ src/app/demo/directive/directive.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class DirectiveComponent implements OnInit {
 
+  // ngClass 可以 动态添加或移除 css class
+  // 根据不同class 显示不同的样式
+  stringProperty = "big-size has-border"
+
+  arrayProperty = ['is-blue', 'has-border']
+
+  objectProperty = { 'is-blue': true, 'big-size': true, 'has-border': false }
+
+  /**
+   * ●NgStyle指令接收键/值对作为输入表达式，前提条件是键必须是有效的属性名称。
+   * ●NgStyle指令可以绑定组件类中的属性。
+   * ●NgStyle指令将会重写当前元素的内置样式。
+   */
+  // 相当于动态地设置 css 样式 
+  styles = { 'border': '2px solid #000', 'color': 'green' }
+
+  styler = {
+    // [WIDTH]: "250", // Use constants
+    "max-width.px": "600", // Hard-Coding
+    "min-width.px": "200",
+    "filter": "sepia(0)"
+  }
+
+
   show: boolean = false
 
   items = ["java", "kotlin", "android", "nodejs", "angular"]
