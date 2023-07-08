75de994c9eab240a1097c72f25c33802924b1e01
shakespace <zez160089@163.com>
2023-07-08 14:23:21 +0800

    1.1 Angular  组件 ｜ 基本数据绑定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div>{{message}}</div>`,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'learn-ang';
+export class AppComponent implements AfterViewInit {
+  message = "My Message"
+
+  ngAfterViewInit(): void {
+    // 每一秒 更新一次 message
+    // setInterval(() => this.message = Date.now().toString(), 1000)
+  }
 }
+
