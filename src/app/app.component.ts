1da33ad7907feececa0303edbef0c3ae6c9485d0
shakespace <zez160089@163.com>
2023-07-08 14:49:18 +0800

    2.2  ViewChild ViewChildren 的基本使用 父视图调用子视图方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { PipeDemoComponent } from './demo/pipe-demo/pipe-demo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('pipe')
+  pipe: PipeDemoComponent
+
   items = {
-    href:"https://angular.io/tutorial",
-    content:"nothing"
+    href: "https://angular.io/tutorial",
+    content: "nothing"
   }
 
-  show(content:string){
+  show(content: string) {
     alert(content)
+  }
+
+  ngAfterViewInit(): void {
+    this.pipe.callPhone("from parent")
   }
 
 
--- src/app/demo/pipe-demo/pipe-demo.component.ts
+++ src/app/demo/pipe-demo/pipe-demo.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-pipe-demo',
   templateUrl: './pipe-demo.component.html',
   styleUrls: ['./pipe-demo.component.css']
 })
-export class PipeDemoComponent implements OnInit {
+export class PipeDemoComponent implements OnInit, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked {
 
   person = {
     name: "Tom",
     age: 23
   }
+
+  name: string = "Testing"
 
   today = Date()
 
@@ -20,7 +23,41 @@ export class PipeDemoComponent implements OnInit {
 
   constructor() { }
 
+  // 配置了 static - true title2 在ngOnInit时就可以取到 ，如果没有 {{name}} 绑定，可以正常获取到  Hello
+  // 但在当前版本 {{name}} 在 ngOnInit 时还没有绑定上，debug 显示此时 interHTML 是  “”
+  // ngAfterViewInit 都可以获取到内容  
   ngOnInit(): void {
+    console.log("ngOnInit --- title1 = " + this.getTitleValue(this.title1));
+    console.log("ngOnInit --- title2 = " + this.getTitleValue(this.title2));
+    console.log("ngOnInit --- title2 nativeElement = " + this.title2.nativeElement);
+
+    console.log("ngOnInit --- titles = " + this.titiles);
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit --- title1 = " + this.getTitleValue(this.title1));
+    console.log("ngAfterContentInit --- title2 = " + this.getTitleValue(this.title2));
+
+    console.log("ngAfterContentInit --- titles = " + this.titiles);
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked --- title1 = " + this.getTitleValue(this.title1));
+    console.log("ngAfterContentChecked --- title2 = " + this.getTitleValue(this.title2));
+
+    console.log("ngAfterContentChecked --- titles = " + this.titiles);
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit --- title1 = " + this.getTitleValue(this.title1));
+    console.log("ngAfterViewInit --- title2 = " + this.getTitleValue(this.title2));
+
+    console.log("ngAfterViewInit --- titles = " + this.titiles.length);
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked --- title1 = " + this.getTitleValue(this.title1));
+    console.log("ngAfterViewChecked --- title2 = " + this.getTitleValue(this.title2));
+
+    console.log("ngAfterViewChecked --- titles = " + this.titiles.length);
   }
 
   callPhone(phone: string) {
@@ -29,6 +66,22 @@ export class PipeDemoComponent implements OnInit {
 
   printName(phone: string) {
     console.log("username  is " + phone);
+  }
+
+  //--------------- ViewChild & ViewChildren--------
+  @ViewChild('title1', { static: false })
+  title1: ElementRef
+
+  @ViewChild('title2', { static: true })
+  title2: ElementRef
+
+  // 获取多个元素 
+  @ViewChildren('title1,title2')
+  titiles: ElementRef[]
+
+  // 元素不为空 则返回元素的文本内容 
+  getTitleValue(v: ElementRef) {
+    return v ? v.nativeElement.innerHTML : v
   }
 
 }
