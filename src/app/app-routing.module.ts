41dc0605cb77730abde2fe9e9eb6e78d5094bde9
shakespace <zez160089@163.com>
2023-07-09 13:11:35 +0800

    5.2 路由 | 路由的基本配置和使用

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LiShowComponent } from './demo/li-show/li-show.component';
+import { LifecycleComponentComponent } from './demo/lifecycle-component/lifecycle-component.component';
+import { PipeDemoComponent } from './demo/pipe-demo/pipe-demo.component';
+import { DirectiveComponent } from './demo/directive/directive.component';
 
-const routes: Routes = []
+// 添加路由配置
+const routes: Routes = [
+  { path: "home", component: AppComponent },
+  { path: "lesson1", component: LiShowComponent },
+  { path: "lesson2", component: LifecycleComponentComponent },
+  { path: "lesson3", component: PipeDemoComponent },
+  { path: "lesson4", component: DirectiveComponent },
+  { path: "**", redirectTo: "home" },
+]
 
 @NgModule({
   declarations: [],
